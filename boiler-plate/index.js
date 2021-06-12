83eee825612886352b5971f719967893d1f45290
이지호 <cctv1998@naver.com>
2021-06-12 18:05:33 +0900

    start

---
+++ boiler-plate/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://jiho:950926asd@boilerplate.5lx3w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify:false
+}).then(()=>console.log("mongoDB Connected..."))
+.catch(err => console.log(err))
+ 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
