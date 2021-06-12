5ecc4440f79a1cf0402601c11570373f35210985
이지호 <cctv1998@naver.com>
2021-06-12 19:44:03 +0900

    비밀 정보 보호

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,16 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jiho:950926asd@boilerplate.5lx3w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
-    useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify:false
-}).then(()=>console.log("mongoDB Connected..."))
-.catch(err => console.log(err))
- 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const express = require("express");
+const app = express();
+const port = 5000;
+const { User } = require("./models/User.js");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const config = require('./config/key')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose
+  .connect(config.mongoURI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("mongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World! 아령하세연");
+});
+
+app.post("/register", (req, res) => {
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다
+
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
