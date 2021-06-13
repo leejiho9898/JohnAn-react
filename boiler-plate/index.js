1cdab7f316d9b3dcd5a6410af91b7bc9f876e1a0
이지호 <cctv1998@naver.com>
2021-06-13 13:08:40 +0900

    비밀번호 암호화,이메일 대조

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -25,8 +25,8 @@ app.get("/", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  //회원 가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다
 
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다
   const user = new User(req.body);
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
@@ -35,7 +35,29 @@ app.post("/register", (req, res) => {
     });
   });
 });
+app.post("/login", (req,res)=>{
+  //요청된 이메일을 데이터베이스에 있는지 확인한다.
+  User.findOne({email:req.body.email},(err,userInfo)=>{
+    if(!userInfo){
+      return res.json({
+        loginSuccess:false,
+        message:"제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+  //요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+    user.comparePassword(req.body.password,(err,isMatch)=>{
+      if(!isMatch)
+      return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."})
 
+        //비밀번호까지 맞다면 토큰생성.
+        user.generateToken((err,user)=>{
+          
+        })
+    })
+  })
+
+
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -1,31 +1,62 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const userSchema = mongoose.Schema({
-    name : {
-        type:String,
-        maxlength:50
-    },
-    email : {
-        type:String,
-        minlength:5
-    },
-    lastname:{
-        type:String,
-        minlength:5
-    },
-    role:{
-        type:Number,
-        default:0
-    },   
-    image:String,
-    token :{
-        type:String
-    },
-    tokenExp:{
-        type:Number
-    }
-})
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    minlength: 5,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    minlength: 5,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
 
-const User = mongoose.model('User',userSchema)
+userSchema.pre("save", function (next) {
+  var user = this;
 
-module.exports={User}
+  if (user.isModified("password")) {
+    //비밀번호를 암호화시킨다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+user.methods.comparePassword = function(plainPassword,cd){
+    //plainPassworld 1234asd  암호화된 비밀번호 $2b$10$/bSZS9zTi8m8X9n44VDHyudUo6FISk6qGlpHnENd7lYcanVtk/S5q
+    bcrypt.compare(plainPassword,this.password,function(err,isMatch){
+        if (err) return cd(err),
+        cd(null,isMatch)
+    })
+}
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
