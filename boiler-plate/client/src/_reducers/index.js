086bace388340f8ca6377c334b9ae83e18d2bbc9
이지호 <cctv1998@naver.com>
2021-06-17 04:04:12 +0900

    redux

---
+++ boiler-plate/client/src/_reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import user from './user_reducer';
+import comment from './comment/'
+const rootReducer = combineReducers({
+    //user
+})
+
+export default rootReducer;
--- boiler-plate/client/src/index.js
+++ boiler-plate/client/src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-import 'antd/dist/antd.css';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Profiler, Provider } from "react-redux";
+import reportWebVitals from "./reportWebVitals";
+import "antd/dist/antd.css";
+import { applyMiddleware } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import { Reducer } from "./_reducers";
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider
+    store={createStoreWithMiddleware(
+      Reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <App />,
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
